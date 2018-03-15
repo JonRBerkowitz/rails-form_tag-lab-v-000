eea59fb0182bfce14fec5035fec6641b849e3a76
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-03-15 03:24:52 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,4 +12,9 @@ class StudentsController < ApplicationController
 
   end
 
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  end
+
 end
+title: params[:post][:title], description: params[:post][:description]
